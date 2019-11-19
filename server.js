5371d8429565700b118b85eaf7c4f1f399593d40
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-11-19 11:30:00 -0700

    Testing

--- server.js
+++ server.js
@@ -17,29 +17,44 @@ app.listen(app.get('port'), function() {
   });
 
 
-function getPerson(req, res) {
+  function getPerson(request, response) {
+	// First get the person's id
+	const id = request.query.id;
 
-    var person_id = req.query.id;
+	// TODO: We should really check here for a valid id before continuing on...
 
-	getPersonFromDb(person_id, function(err, res) {
+	// use a helper function to query the DB, and provide a callback for when it's done
+	getPersonFromDb(id, function(error, result) {
+		// This is the callback function that will be called when the DB is done.
+		// The job here is just to send it back.
 
-		if (err || res == null || res.length != 1) {
-			res.status(500).json({success: false, data: err});
+		// Make sure we got a row with the person, then prepare JSON to send back
+		if (error || result == null || result.length != 1) {
+			response.status(500).json({success: false, data: error});
 		} else {
-			const person = res[0];
-			res.status(200).json(person);
+			const person = result[0];
+			response.status(200).json(person);
 		}
 	});
 }
 
+// This function gets a person from the DB.
+// By separating this out from the handler above, we can keep our model
+// logic (this function) separate from our controller logic (the getPerson function)
 function getPersonFromDb(id, callback) {
 	console.log("Getting person from DB with id: " + id);
 
-	const sql = "SELECT id, first_name, last_name, birth_date FROM person WHERE id = $1::int";
+	// Set up the SQL that we will use for our query. Note that we can make
+	// use of parameter placeholders just like with PHP's PDO.
+	const sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";
 
+	// We now set up an array of all the parameters we will pass to fill the
+	// placeholder spots we left in the query.
 	const params = [id];
 
-	pool.query(sql, params, function(err, res) {
+	// This runs the query, and then calls the provided anonymous callback function
+	// with the results.
+	pool.query(sql, params, function(err, result) {
 		// If an error occurred...
 		if (err) {
 			console.log("Error in query: ")
@@ -48,9 +63,15 @@ function getPersonFromDb(id, callback) {
 		}
 
 		// Log this to the console for debugging purposes.
-		console.log("Found result: " + JSON.stringify(res.rows));
+		console.log("Found result: " + JSON.stringify(result.rows));
 
-		callback(null, res.rows);
+
+		// When someone else called this function, they supplied the function
+		// they wanted called when we were all done. Call that function now
+		// and pass it the results.
+
+		// (The first parameter is the error variable, so we will pass null.)
+		callback(null, result.rows);
 	});
 
 }
