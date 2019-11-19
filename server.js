99eb9d2b7ea64e0f0935b2426412cbf1cc4eb32a
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-11-19 11:17:17 -0700

    Added stuff

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const { Pool } = require("pg");
@@ -8,19 +7,64 @@ const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connectionString});
 var sql = "SELECT * FROM.person";
 
-pool.query(sql, function(err, result) {
-    // If an error occured...
-    if(err) {
-        console.log("Error in query: ")
-        console.log(err);
-    }
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-    // Log this to the console for debugging purposes.
-    console.log("Back from DB with result: ");
-    console.log(result.rows);
-
-});
+app.get('/getPerson', getPerson);
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
   });
+
+
+function getPerson(req, res) {
+
+    var person_id = request.query.id;
+
+	getPersonFromDb(id, function(err, res) {
+
+		if (err || res == null || res.length != 1) {
+			res.status(500).json({success: false, data: err});
+		} else {
+			const person = result[0];
+			res.status(200).json(person);
+		}
+	});
+}
+
+function getPersonFromDb(id, callback) {
+	console.log("Getting person from DB with id: " + id);
+
+	const sql = "SELECT id, first_name, last_name, birth_date FROM person WHERE id = $1::int";
+
+	const params = [id];
+
+	pool.query(sql, params, function(err, res) {
+		// If an error occurred...
+		if (err) {
+			console.log("Error in query: ")
+			console.log(err);
+			callback(err, null);
+		}
+
+		// Log this to the console for debugging purposes.
+		console.log("Found result: " + JSON.stringify(res.rows));
+
+		callback(null, result.rows);
+	});
+
+}
+
+// pool.query(sql, function(err, result) {
+//     // If an error occured...
+//     if(err) {
+//         console.log("Error in query: ")
+//         console.log(err);
+//     }
+
+//     // Log this to the console for debugging purposes.
+//     console.log("Back from DB with result: ");
+//     console.log(result.rows);
+
+// });
+
--- tables.sql
+++ tables.sql
@@ -4,3 +4,8 @@ CREATE TABLE person (
     last_name VARCHAR(80),
     birth_date DATE
 );
+
+INSERT INTO person(first_name, last_name, birth_date) VALUES
+('Merrritt', 'Creighton', '1970-08-22'),
+('Charles', 'Creighton', '1969-06-22'),
+('Rufus', 'Dalbini', '1920-10-12');
