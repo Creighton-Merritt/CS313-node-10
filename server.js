9ec5f8e35d8aa2d58902fb4b16247559f736e135
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-11-20 12:51:49 -0700

    server.js

--- server.js
+++ server.js
@@ -15,21 +15,70 @@ app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
   });
 
-var sql = "SELECT * FROM person";
-
-function getPerson() {
-    pool.query(sql, function(err, result) {
-    // If an error occured...
-    if(err) {
-        console.log("Error in query: ")
-        console.log(err);
-    }
-
-    // Log this to the console for debugging purposes.
-    console.log("Back from DB with result: ");
-    console.log(result.rows);
-    result.rows;
-
-});
+function getPerson(request, response) {
+   const id = request.query.id;
+        getPersonFromDb(id, function(error, result) {
+    
+            if (error || result == null || result.length != 1) {
+                response.status(500).json({success: false, data: error});
+            } else {
+                const person = result[0];
+                response.status(200).json(person);
+            }
+        });
 }
 
+function getPersonFromDb(id, callback) {
+	console.log("Getting person from DB with id: " + id);
+
+	// Set up the SQL that we will use for our query. Note that we can make
+	// use of parameter placeholders just like with PHP's PDO.
+	const sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";
+
+	// We now set up an array of all the parameters we will pass to fill the
+	// placeholder spots we left in the query.
+	const params = [id];
+
+	// This runs the query, and then calls the provided anonymous callback function
+	// with the results.
+	pool.query(sql, params, function(err, result) {
+		// If an error occurred...
+		if (err) {
+			console.log("Error in query: ")
+			console.log(err);
+			callback(err, null);
+		}
+
+		// Log this to the console for debugging purposes.
+		console.log("Found result: " + JSON.stringify(result.rows));
+
+
+		// When someone else called this function, they supplied the function
+		// they wanted called when we were all done. Call that function now
+		// and pass it the results.
+
+		// (The first parameter is the error variable, so we will pass null.)
+		callback(null, result.rows);
+	});
+
+} // end of getPersonFromDb
+
+
+// test from activity
+// var sql = "SELECT * FROM person";
+
+// function getPerson() {
+//     pool.query(sql, function(err, result) {
+//     // If an error occured...
+//     if(err) {
+//         console.log("Error in query: ")
+//         console.log(err);
+//     }
+
+//     // Log this to the console for debugging purposes.
+//     console.log("Back from DB with result: ");
+//     console.log(result.rows);
+//     result.rows;
+
+// });
+// }
--- tables.sql
+++ tables.sql
@@ -8,4 +8,26 @@ CREATE TABLE person (
 INSERT INTO person(first_name, last_name, birth_date) VALUES
 ('Merrritt', 'Creighton', '1970-08-22'),
 ('Charles', 'Creighton', '1969-06-22'),
-('Rufus', 'Dalbini', '1920-10-12');
+('Rufus', 'Dalbini', '1920-10-12'),
+('Judy', 'Tuley', '1920-10-12'),
+('Ethne', 'Creighton', '2004-05-13'),
+('Lori', 'Creighton', '1950-12-07'),
+('Ruth', 'Thurman', '1920-09-09');
+
+CREATE TABLE relationship (
+    parent_id INTEGER REFERENCES person(id),
+    child_id INTEGER REFERENCES person(id)
+);
+
+INSERT INTO relationship(parent_id, child_id) VALUES
+(3, 4),
+(7, 6),
+(6, 2),
+(4, 1),
+(1, 5),
+(2, 5);
+
+SELECT DISTINCT a.first_name, b.first_name, parent_id, child_id
+FROM person a, person b, relationship
+WHERE parent_id = a.id
+and child_id = b.id;
