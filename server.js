4591f1aab7d3960b1c1c4c0e392001053c25605e
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-11-20 14:35:36 -0700

    server

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ function getPerson(req, res) {
 
         getPersonFromDb(id, function(error, result) {
             console.log("Back from the database with result: ", result);
+            res.json(result);
         });
 
     
@@ -54,32 +55,6 @@ function getPersonFromDb(id, callback) {
     });
 
 }
-// 	console.log("Getting person from DB with id: " + id);
-
-// 	// Set up the SQL that we will use for our query. Note that we can make
-// 	// use of parameter placeholders just like with PHP's PDO.
-// 	const sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";
-
-// 	// We now set up an array of all the parameters we will pass to fill the
-// 	// placeholder spots we left in the query.
-// 	const params = [id];
-
-// 	// This runs the query, and then calls the provided anonymous callback function
-// 	// with the results.
-
-// 		// Log this to the console for debugging purposes.
-// 		console.log("Found result: " + JSON.stringify(result.rows));
-
-
-// 		// When someone else called this function, they supplied the function
-// 		// they wanted called when we were all done. Call that function now
-// 		// and pass it the results.
-
-// 		// (The first parameter is the error variable, so we will pass null.)
-// 		callback(null, result.rows);
-// 	});
-
-// } // end of getPersonFromDb
 
 
 // test from activity
