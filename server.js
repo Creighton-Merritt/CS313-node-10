404879543aef532923c29d028160917bce7b1f19
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-11-21 12:12:00 -0700

    Added routing

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/getPerson', getPerson);
 
 app.use(express.static(__dirname + '/public'));
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+
+// still need to install ejs
+// app.set('views', path.join(__dirname, 'views'))
+// app.set('view engine', 'ejs')
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
