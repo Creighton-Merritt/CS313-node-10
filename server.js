59fc1ff066e9e9311f672d478cdea180ea617b3a
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-11-21 12:09:47 -0700

    Add routing

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const app = express();
 const { Pool } = require("pg");
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connectionString});
-
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/getPerson', getPerson);
 
 app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
