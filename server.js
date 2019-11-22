5f46720d1595e892e05644265f4133ee0aac3e12
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-11-22 12:10:28 -0700

    added nodemon to dependencies

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
 // app.set('view engine', 'ejs')
 
 app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running with nodemon on port', app.get('port'));
   });
 
 
