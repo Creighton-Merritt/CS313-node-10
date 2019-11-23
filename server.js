fce8e6d479ba48080972e6c25728e3b2c5c3b12f
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-11-23 14:41:46 -0700

    update list

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path')
 const express = require('express');
 const app = express();
 const { Pool } = require("pg");
@@ -6,12 +7,13 @@ const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connectionString});
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/list', getList);
-//set up default page to select lists
-
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+
+app.get('/list', getList);
+//set up default page to select lists
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running with nodemon on port', app.get('port'));
@@ -52,23 +54,3 @@ function getListFromDB(callback) {
 
 }
 
-
-// require('dotenv').config();
-// const express = require('express');
-// const app = express();
-// // const { Pool } = require("pg");
-// // const connectionString = process.env.DATABASE_URL;
-// // const pool = new Pool({connectionString: connectionString});
-// const getList = (require('./files/getList.js'))
-// app.set('port', (process.env.PORT || 5000));
-
-// app.get('/list', getList);
-// //have form route to different store lists
-
-// app.use(express.static(__dirname + '/public'));
-// app.set('views', path.join(__dirname, 'views'))
-// app.set('view engine', 'ejs')
-
-// app.listen(app.get('port'), function() {
-//     console.log('Node app is running with nodemon on port', app.get('port'));
-//   });
