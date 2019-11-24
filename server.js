08fe80eaea53a1b53314931a96f027db17d015ab
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-11-24 16:06:54 -0700

    .

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/', function(req, res) {
-    res.render('pages/chooseList')
-});
 
 app.get('/list', getFullList);
 app.get('/stores', getStoreList);
+app.get('/', function(req, res) {
+    res.render('pages/chooseList')
+});
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running with nodemon on port', app.get('port'));
