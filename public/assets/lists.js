5014e1d32f861eb03328d8f42f8b59d6e7da0b43
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-11-29 09:58:31 -0700

    Add

--- public/assets/lists.js
+++ public/assets/lists.js
@@ -51,16 +51,19 @@ function addToDb(req, res) {
     const item_name = req.query.item_name;
     console.log("Info from form: " + id);
     console.log("Info from form: " + item_name);
+
+    //add item to db then add to page
     
-    // getListByStore(id, function(error, result) {
-    //     if (error || result == null) {
-    //         res.status(500).json({success:false, data: error});
-    //     } else {
-    //         console.log("Back from the database with store result: ", result);
-    //         const store_id = result[0].store_id;
-    //         console.log("Store id: " + store_id);
-    //         const params = {result: result, store_id: store_id};
-    //         res.render('pages/results', params);
-    //     }
-    // });
+    getListByStore(id, function(error, result) {
+        if (error || result == null) {
+            res.status(500).json({success:false, data: error});
+        } else {
+            console.log("Back from the database with store result: ", result);
+            const store_id = result[0].store_id;
+            console.log("Store id: " + store_id);
+            const params = {result: result, store_id: store_id};
+            res.render('pages/results', params);
+        }
+    });
 }
+
