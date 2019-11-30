899cd31f92a99bbdad28feb6afb4e9b4f0342de0
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-11-29 17:12:10 -0700

    Test add

--- public/assets/results.js
+++
@@ -1,8 +0,0 @@
-function addItemToTable() {
-    var item = document.getElementById(itemName).innerHTML;
-    const table = document.getElementById('#table');
-    const rowNumber = table.rows.length + 1;
-
-    const newRow = '<tr><td>' + rowNumber + '</td><td>' + item + '</td><tr>';
-    table.insertAdjacentHTML('beforeEnd', newRow);
-}
