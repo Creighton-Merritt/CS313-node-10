762b156e05292ce3056a7cf6aae8618992244bd4
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-12-05 14:33:01 -0700

    Correcting links

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 const path = require('path')
 const express = require('express');
 const app = express();
-const getFullList = (require('/fullList.js'));
-const getStoreList = (require('/lists.js'));
 //const bodyParser= require("body-parser");
 
 app.set('port', (process.env.PORT || 5000));
@@ -11,6 +9,8 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.json());       
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')))
+const getFullList = (require('fullList.js'));
+const getStoreList = (require('lists.js'));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
