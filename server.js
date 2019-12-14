4bb0822fc0e1a7521a8975a99251dfbf23014e44
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-12-14 13:55:32 -0700

    .

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const getStoreList = (require('./public/lists.js'));
 const bodyParser= require("body-parser");
 
 app.set('port', (process.env.PORT || 5000));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.json());       
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
