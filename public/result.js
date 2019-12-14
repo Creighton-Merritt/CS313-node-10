7ee4de35567b14584c43c5bca29fa4aae530d7b1
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-12-14 15:42:49 -0700

    .

--- public/result.js
+++ public/result.js
@@ -45,17 +45,24 @@ $(document).ready(() => {
    });
 });
 
-//             if (result.success) {
-//                 console.log("success in storeLists", result);
-                
-//                 var rows = JSON.parse(result);
-//                 for (i=0 ; i < rows.length ; i++){
-//                     var num = (i + 1);
-//                     $('#tableBody').append('<tr><th scope="row">' + num + '</th><td class="text-left">' + result[i].item_name + '</td></tr>');
-//                 }
-//             }
-//         });
-//     });
-// });
 
-
+$(document).ready(() => {
+    $('#storeSubmit').click(() => {
+        const requestURL = 'stores/' + $('#stores :selected').val();
+        console.log("Request url", requestURL);
+        $.ajax({
+            url: requestURL,
+            type: 'GET',
+            dataType: 'json',
+            success: (result) => {
+                $('#tableBody').html("");
+                $('storeName').html(result[0].store_name);
+                console.log('ajax success!', result);
+                for (i=0 ; i < result.length ; i++) {
+                    var num = (i + 1);
+                    $('#tableBody').append('<tr><th scope="row">' + num + '</th><td class="text-left">' + result[i].item_name + '</td></tr>');
+                }
+            }
+        });
+   });
+});
