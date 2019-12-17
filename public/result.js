bc8bf3bef8ca5880b86e0c0cb5b06acf07db009d
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-12-17 11:08:14 -0700

    Test delete rows

--- public/result.js
+++ public/result.js
@@ -56,27 +56,45 @@ $(document).ready(() => {
         $('.checkitem').prop("checked", $(this).prop("checked"))
     })
     $('#delsel').click(function() {
-        if($(this).is(":checked")) {
-            $(this.parents("tr").remove());
-        }
         var item_ids = $('.checkitem:checked').map(function() {
             return $(this).val();
         }).get();
-
         
         var params = {
             item_ids: item_ids,
         };
+
         console.log("Delete params", params);
 
-        // $.post("/deleteFromDB", params, function(result) {
-        //     if (result.success) {
-        //         console.log(result);
-        //         console.log("Deleted");
-        //     } else {
-        //         $('#tableBody').text("Error");
-        //     }
-        // });
+        $.post("/deleteFromDB", params, function(result) {
+            if (result.success) {
+                console.log(result);
+                console.log("Deleted");
+                const requestURL = 'stores/' + $('#storeName').text();
+                console.log("Request url", requestURL);
+                $.ajax({
+                    url: requestURL,
+                    type: 'GET',
+                    dataType: 'json',
+                    success: (result) => {
+                        $('#tableBody').html("");
+                        $('#storeName').html(result[0].store_name);
+                        console.log('ajax success!', result);
+                        for (i=0 ; i < result.length ; i++) {
+                            var num = (i + 1);
+                            $('#tableBody').append('<tr><th scope="row">' + num + '</th><td class="text-left">' + result[i].item_name + 
+                            '</td><td><input type="checkbox" class="checkitem" value="' + result[i].item_id + '"></td></tr>');
+                        }
+                        
+                        $('#stores').prop('selectedIndex', null);
+                        $('#hiddenStoreId').attr("value", result[0].store_id);
+                        $('#adding').css("visibility", "visible");
+                    }
+                });
+            } else {
+                $('#tableBody').text("Error");
+            }
+        });
 
     });
 });
